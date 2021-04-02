c41fd5d0b9745f65713eebff53db0fa16f9f7b23
AleBuser <alebuser.98@gmail.com>
2021-04-02 17:41:18 +0200

    Add a few more PoolController tests

--- test/PoolController.test.ts
+++ test/PoolController.test.ts
@@ -2,29 +2,38 @@ import { ethers } from 'hardhat';
 import { BigNumber, Signer } from 'ethers';
 import { expect } from 'chai';
 import * as helpers from './helpers/helpers';
+
 import { 
-    Erc20Mock, BasketBalancerMock, PoolController
+    Erc20Mock, BasketBalancerMock, PoolController, Pool
  } from '../typechain';
 import * as deploy from './helpers/deploy';
+import { stringify } from 'querystring';
 
 
 describe('PoolController', function () {
 
-    let  sov: Erc20Mock,reign: Erc20Mock, underlying: Erc20Mock
+    let  sov: Erc20Mock,reign: Erc20Mock, underlying1: Erc20Mock, underlying2: Erc20Mock
     let  balancer: BasketBalancerMock, pool_controller:PoolController;
+    let pool:Pool;
 
     let user: Signer, userAddress: string;
     let reignDAO: Signer, reignDAOAddress: string;
 
+    let pool_address:string;
+
     before(async function () {
         sov = (await deploy.deployContract('ERC20Mock')) as Erc20Mock;
         reign = (await deploy.deployContract('ERC20Mock')) as Erc20Mock;
-        underlying = (await deploy.deployContract('ERC20Mock')) as Erc20Mock; 
+        underlying1 = (await deploy.deployContract('ERC20Mock')) as Erc20Mock; 
+        underlying2 = (await deploy.deployContract('ERC20Mock')) as Erc20Mock; 
+ 
 
-        balancer = (await deploy.deployContract('BasketBalancerMock',[[], []])) as BasketBalancerMock; 
+        balancer = (await deploy.deployContract('BasketBalancerMock',[[underlying1.address], [1000000]])) as BasketBalancerMock; 
 
         await setupSigners();
         await setupContracts();
+
+
 
         
     });
@@ -33,6 +42,14 @@ describe('PoolController', function () {
         pool_controller = (
             await deploy.deployContract('PoolController', [balancer.address, sov.address, reign.address, reignDAOAddress])
         ) as PoolController; 
+
+        await pool_controller.connect(reignDAO).createPool(underlying1.address)
+
+        pool_address = await pool_controller.allPools(0);
+
+        //connect to deployed pool
+        pool = (await deploy.deployContract('Pool')) as Pool;
+        pool = pool.attach(pool_address);
     })
 
 
@@ -41,35 +58,64 @@ describe('PoolController', function () {
             expect(pool_controller.address).to.not.eql(0).and.to.not.be.empty;
         });
     
-    }); 
+    });
+    
     
     describe('Creating Pools', async function () {
 
+        
         it('creates a pool for an underlying', async function () {
-            let pool_address = await pool_controller.connect(reignDAO).createPool(underlying.address);
-            expect(pool_address).to.not.eql(0).and.to.not.be.empty;
-        });
+            expect(await pool.token()).to.eq(underlying1.address)
+        })
 
 
         it('adds the pools to the pool list', async function () {
             let pool_len = await pool_controller.allPoolsLength();
-            expect(pool_len).to.eq(0);
-            await expect(pool_controller.connect(reignDAO).createPool(underlying.address)).to.not.be.reverted;
+            expect(pool_len).to.eq(1);
+            await expect(pool_controller.connect(reignDAO).createPool(underlying2.address)).to.not.be.reverted;
             let pool_len_after = await pool_controller.allPoolsLength();
-            expect(pool_len_after).to.eq(1);
+            expect(pool_len_after).to.eq(2);
         });
 
-        it('adds the pool to the mapping', async function () {
-            await expect(pool_controller.connect(reignDAO).createPool(underlying.address)).to.not.be.reverted;
+
+        it('adds the pools to the balancer list', async function () {
+            await expect(pool_controller.connect(reignDAO).createPool(underlying2.address)).to.not.be.reverted;
+            let all_pools = await balancer.getPools();
+            expect(all_pools[0]).to.eq(await pool.token());
+        });
+
+        it('adds the pools to the mapping', async function () {
+            await expect(pool_controller.connect(reignDAO).createPool(underlying2.address)).to.not.be.reverted;
             let last_pool = await pool_controller.allPools(0);
-            let new_pool = await pool_controller.getPool(underlying.address);
+            let new_pool = await pool_controller.getPool(underlying1.address);
+            expect(new_pool).to.eq(last_pool);
+            last_pool = await pool_controller.allPools(1);
+            new_pool = await pool_controller.getPool(underlying2.address);
             expect(new_pool).to.eq(last_pool);
         });
 
+
+        it('correctly relays taregt allocation', async function () {
+            expect(await pool_controller.getTargetAllocation(underlying2.address)).to.eq(1000000);
+        });
+
+        it('correctly relays interest rate', async function () {
+            let interest_rate = await pool_controller.getInterestRate(pool_address,100,101)
+            expect(interest_rate[0]).to.eq(338497452615);
+            expect(interest_rate[1]).to.eq(0);
+
+            interest_rate = (await pool_controller.getInterestRate(pool_address,101,100))
+            expect(interest_rate[0]).to.eq(57305936073);
+            expect(interest_rate[1]).to.eq(0);
+        });
+
+        it('correctly returns on isPool', async function () {
+            expect(await pool_controller.isPool(pool_address)).to.be.true;
+            expect(await pool_controller.isPool(userAddress)).to.be.false;
+        });
+
         it('initializes the interest strategy', async function () {
-            await expect(pool_controller.connect(reignDAO).createPool(underlying.address)).to.not.be.reverted;
-            let last_pool = await pool_controller.allPools(0);
-            let interest = await pool_controller.getInterestStrategy(last_pool);
+            let interest = await pool_controller.getInterestStrategy(pool_address);
             expect(interest).to.not.eql(0).and.to.not.be.empty;
         });
 
@@ -86,9 +132,9 @@ describe('PoolController', function () {
         });
 
         it('reverts if pool already exists', async function () {
-            await expect(pool_controller.connect(reignDAO).createPool(underlying.address)).to.not.be.reverted;
+            await expect(pool_controller.connect(reignDAO).createPool(underlying2.address)).to.not.be.reverted;
             await expect( 
-                 pool_controller.connect(reignDAO).createPool(underlying.address)
+                 pool_controller.connect(reignDAO).createPool(underlying2.address)
             ).to.be.revertedWith('SoV-Reign: POOL_EXISTS');
         });
     }); 
@@ -97,7 +143,8 @@ describe('PoolController', function () {
     async function setupContracts () {
         const cvValue = BigNumber.from(2800000).mul(helpers.tenPow18);
 
-        await underlying.mint(userAddress, cvValue);
+        await underlying1.mint(userAddress, cvValue);
+        await underlying2.mint(userAddress, cvValue);
     }
 
     async function setupSigners () {
