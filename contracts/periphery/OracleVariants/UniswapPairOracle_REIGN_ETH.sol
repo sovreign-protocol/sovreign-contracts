e9f06df3cb626d6fdb20656dc098e34d46420b87
Guilherme Sperb Machado <guil@axlabs.com>
2021-04-28 13:08:06 +0200

    (minor) add comments and rename vars

--- contracts/periphery/OracleVariants/UniswapPairOracle_REIGN_ETH.sol
+++ contracts/periphery/OracleVariants/UniswapPairOracle_REIGN_ETH.sol
@@ -5,20 +5,25 @@ import "../UniswapPairOracle.sol";
 
 // Fixed window oracle that recomputes the average price for the entire period once every period
 // Note that the price average is only guaranteed to be over at least 1 period, but may be over a longer period
+
+// factory: the pool factory (e.g., uniswap factory)
+// tokenA and tokenB: addresses for the pair
+// owner: the reignDAO
+// timelock: ?
 contract UniswapPairOracle_REIGN_WETH is UniswapPairOracle {
     constructor(
         address factory,
         address tokenA,
         address tokenB,
-        address owner_address,
-        address timelock_address
+        address owner,
+        address timelock
     )
         UniswapPairOracle(
             factory,
             tokenA,
             tokenB,
-            owner_address,
-            timelock_address
+            owner,
+            timelock
         )
     {}
 }
