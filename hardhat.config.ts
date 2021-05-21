eed3e1d4784d0bbae767959e70e04ae1820c1fe4
AleBuser <alebuser.98@gmail.com>
2021-05-21 15:53:17 +0200

    Updtae config for optimization

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,7 @@ import * as config from './config';
 import '@nomiclabs/hardhat-waffle';
 import '@nomiclabs/hardhat-etherscan';
 import 'hardhat-abi-exporter';
+import 'hardhat-contract-sizer';
 import '@typechain/hardhat';
 import 'solidity-coverage';
 import 'hardhat-gas-reporter';
@@ -25,7 +26,7 @@ const cfg: HardhatUserConfig = {
         settings: {
             optimizer: {
                 enabled: true,
-                runs: 9999,
+                runs: 200,
             },
         },
     },
