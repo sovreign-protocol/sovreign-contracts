9f311c6cdccefdf3208bb8ca0d06b337c8a71ca4
AleBuser <alebuser.98@gmail.com>
2021-05-25 16:33:06 +0200

    Add token contracts

--- scripts/config-rinkeby.ts
+++ scripts/config-rinkeby.ts
@@ -9,9 +9,9 @@ export async function deployConfig(): Promise<DeployConfig> {
     const user1Addr: string = '0xA5E3C2047a28f0C8032B1A7e7074682B129445a7'; // WETH whale
     const user2Addr: string = '0x5577bd667608bBB2537f3d45610B14b7286466a7'; // WBTC whale
     const user3Addr: string = '0x2A95300047E373EEEAa5Eea0be7dcE1418Ccf191'; // Binance
-    const usdcAddr:  string = '';
-    const wbtcAddr:  string = '';
-    const wethAddr:  string = '';
+    const usdcAddr:  string = '0x354F3750D24C294c511027Bd0EcB66047338f887';
+    const wbtcAddr:  string = '0xe561D4C3D1c7141FC5B18830BAe9CDf4Dbae12AF';
+    const wethAddr:  string = '0x5523c63dc7a2fd6b0b8Bb230FbE99c61591811C2';
     const uniswapFactoryAddr: string = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
     const uniswapRouterAddr:  string = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
     const btcChainlinkOracle: string = '0xECe365B379E1dD183B20fc5f022230C044d51404';
