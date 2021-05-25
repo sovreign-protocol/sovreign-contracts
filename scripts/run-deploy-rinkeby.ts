9c43a2050f8cb7e0cd2cc62a0ad7a0a0dee736ff
Guilherme Sperb Machado <guil@axlabs.com>
2021-05-25 16:13:44 +0200

    organize imports

--- scripts/run-deploy-rinkeby.ts
+++ scripts/run-deploy-rinkeby.ts
@@ -1,10 +1,7 @@
-import {mainnetPreFlight} from "./deployment/define-0-mainnet-preflight";
 import {deployDAO} from "./deployment/define-1-deploy-DAO";
 import {tokenSetup} from "./deployment/define-2-tokens-setup";
 import {controllerSetup} from "./deployment/define-3-controller-setup";
 import {activateSoVReign} from "./deployment/define-4-activate-SoVReign";
-import {createPools} from "./deployment/define-5-create-pools";
-import {createRewards} from "./deployment/define-6-create-rewards";
 import {deployConfig} from "./config-rinkeby";
 
 deployConfig()
